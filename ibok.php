a05673d0e9f0d9c184fe1f8a65e3bb230abf503c
Dag Frode <tncditto@gmail.com>
2016-09-05 23:40:51 +0200

    Added ad prices and store prices to the api

--- ibok.php
+++ ibok.php
@@ -1,7 +1,34 @@
 <?php
-$book_page = file_get_contents('https://ibok.no/bok/' . $_GET['isbn']);
+$isbn = $_GET['isbn'];
+$book_page = file_get_contents('https://ibok.no/bok/' . $isbn);
 $book_id = explode("'", explode("{book_id:'", $book_page)[1])[0];
-$json = file_get_contents('https://ibok.no/api/classified/?book_id=' . $book_id$
-$json = '"{bookId: "' . $book_id . '", json:"' . $json;
+$add_json = file_get_contents('https://ibok.no/api/classified/?book_id=' . $book_id );
+$add_json = json_decode($add_json, true);
 
-echo $json;
+
+$details_regexp = "/<p>(?:\s)*<b>Av:<\/b>(.*)<br>(?:\s)*<b>Forlag:<\/b>(.*)<br>(?:\s*)<b>ISBN:<\/b>(.*)<br>(?:\s*)<b>Utgitt:<\/b>(.*)<br>$/m";
+preg_match($details_regexp, $book_page, $matches);
+
+$prices_regexp = '/<span id="(.*)" class=" books_price">(?:\s*)(.*?)(?:\s*)<\/span>/';
+preg_match_all($prices_regexp, $book_page, $prices);
+
+$stores = $prices[1];
+$prices = $prices[2];
+
+$prices_ass = [];
+
+for($i = 0; $i < count($prices); $i++){
+    array_push($prices_ass, ['store' => $stores[$i], 'price' => $prices[$i]]);
+}
+
+$json = [
+    'ads' => $add_json,
+    'isbn' => $isbn,
+    'bookId' => $book_id,
+    'author' => $matches[1],
+    'publisher' => $matches[2],
+    'year' => $matches[4],
+    'prices' => $prices_ass
+];
+header('Content-Type: application/json');
+echo json_encode($json);
