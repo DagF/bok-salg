ec371eb6ff30a5ab02123c8ad9a2e6612c82f38b
Dag Frode <tncditto@gmail.com>
2016-09-06 23:04:59 +0200

    Added base values to table row

--- main.js
+++ main.js
@@ -1,9 +1,50 @@
 
-function getBook(event){
+var API_URL = "https://dagfro.de/projects/bok-salg/ibok.php?isbn=";
+var ELEMENTS = {
+    'books' : document.getElementById("books")
+};
+
+
+function getBook(){
     var isbn = document.getElementById("input-isbn").value;
     console.log(isbn);
-    fetch("https://ibok.no/bok/" + isbn).then(function(response){
-        console.log(response);
-    });
+    fetch(API_URL + isbn).then(function(response){
+       return response.json();
+    }).then(function (json){
+        console.log(json);
+        ELEMENTS.books.innerHTML += toTableRow(json);
+    }
+
+    );
     return false;
+}
+
+function getLowestPriceFromAds(json) {
+    return 5;
+}
+function getHighetsPriceFromAds(json) {
+    return 5;
+
+}
+function getTypicalPriceFromAds(json) {
+    return 5;
+
+}
+function toTableRow(json){
+    var price = {
+        "lowest" : getLowestPriceFromAds(json['ads']),
+        "highest" : getHighetsPriceFromAds(json['ads']),
+        "typical" : getTypicalPriceFromAds(json['ads'])
+    };
+    return "<tr>" +
+        "<td>"+json['title']+"</td>"+
+        "<td>"+json['isbn']+"</td>"+
+        "<td>"+json['author']+"</td>"+
+        "<td>"+json['publisher']+"</td>"+
+        "<td>"+json['year']+"</td>"+
+        "<td>Lavest pris</td>"+
+        "<td>Typisk pris</td>"+
+        "<td>Høyest pris</td>"+
+        "<td><a href='https://ibok.no/bok/'" + json['bookId'] + ">Ibok-link</a></td>"+
+        "</tr>";
 }
